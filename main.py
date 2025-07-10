7cc4e2023087c70778737e75a19664041fc5512a
Egromaster <egromaster@gmail.com>
2025-07-10 23:50:23 +0300

    Update main.py

--- main.py
+++ main.py
@@ -109,7 +109,4 @@ def get_user_data():
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
 
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=80)
-
 
